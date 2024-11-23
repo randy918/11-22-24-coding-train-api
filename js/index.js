6f27316ad88e23a26863093ef0f7f02ff2f1fbdf
Randy Miller <randy918@gmail.com>
2024-11-23 13:56:57 -0600

    good simple fetch example 11-23-24-1356

--- js/index.js
+++ js/index.js
@@ -67,8 +67,30 @@ document.addEventListener("DOMContentLoaded", function () {
     })
         .catch(error => {
             console.error(error);
-            console.log('error');
         });
+
+
+    async function getData() {
+        const response = await fetch('/csv/ZonAnn.Ts+dSST.csv');
+        const data = await response.text();
+        
+        const table = data.split('\n').slice(1);
+        
+        table.forEach(row => {
+        const columns = row.split(',');
+        const year = columns[0];
+        const temp = columns[1];
+        console.log(year, temp);
+        
+        });
+        
+        
+    }
+    
+getData();
+
+
+
 
 });
 
