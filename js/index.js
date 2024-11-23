c66abeacf8131e4a8cf9028ab730a555792210e8
Randy Miller <randy918@gmail.com>
2024-11-23 11:46:03 -0600

    11-23-24-1133 getting firat async await

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
+//<  ████████████████████████████████████  LOADER
+
 document.addEventListener("DOMContentLoaded", function () {
     // Your code here
 
     document.body.style.backgroundColor = 'green';
 
-    //>  ████████████████████████████████████  VARIABLES
+    //>  ████████████████████████████████████  CHILDREN
 
-   
-    //>  ████████████████████████████████████  LISTENERS
 
-  
 
-    //>  ████████████████████████████████████  BRIGHT BACKGROUND
+
+
+
+
+
+    //>  ████████████████████████████████████  GENERAL FUNCTIONS
 
     function getBrightRandomRGB() {
         const rrr = r(86, 255);
@@ -19,22 +23,68 @@ document.addEventListener("DOMContentLoaded", function () {
         const rgbColor = `rgb(${rrr}, ${ggg}, ${bbb})`;
         return rgbColor;
     }
-    
+
+
+    let pictures = ["/img/ashutterstock_2260187227.jpg", "/img/ashutterstock_2274444143.jpg", "/img/ashutterstock_2290464509.jpg", "/img/ashutterstock_2308412739.jpg", "/img/ashutterstock_2325872667.jpg", "/img/ashutterstock_2402783767.jpg", "/img/shutterstock_2260187227.jpg", "/img/shutterstock_2274444143.jpg", "/img/shutterstock_2290464509.jpg", "/img/shutterstock_2308412739.jpg", "/img/ashutterstock_497267167.jpg", "/img/shutterstock_2402783767.jpg", "/img/ashutterstock_497764618.jpg", "/img/shutterstock_2402783767.jpg",]
+
+    function getRandomImg() {
+        let randomPicture = pictures[r(pictures.length)];
+        console.log(randomPicture);
+        return randomPicture;
+    }
+
+
+
+
+    //_ ████████████████████████████████████  DATA STRUCTURES 
+
+    //_ ████████████████████████████████████  DATA STRUCTURES 
+
+
+
+
+    //>  ████████████████████████████████████  FUNCTIONS
+
+
+    console.log('About to fetch a rainbow');
+
+    async function catchRainbow() {
+        const response = await fetch(getRandomImg());
+        const blob = await response.blob();
+        document.getElementById('manly').src = URL.createObjectURL(blob);
+
+
+    }
+
+
+
+    //>  ████████████████████████████████████  GENERAL PROGRAM
+
     document.body.style.backgroundColor = getBrightRandomRGB();
 
-//_ ████████████████████████████████████  DATA STRUCTURES 
+    catchRainbow().then(response => {
+        console.log('yay');
+    })
+        .catch(error => {
+            console.error(error);
+            console.log('error');
+        });
+
+});
 
 
 
-//_ ████████████████████████████████████  DATA STRUCTURES 
 
 
 
-//>  ████████████████████████████████████  GENERAL PROGRAM
 
 
 
-    //< ████████████████████████████████████  MISC
 
-})
+
+// console.log(document.documentElement);
+
+//< ████████████████████████████████████  MISC
+
+
 
